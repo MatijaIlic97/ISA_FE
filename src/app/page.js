0d84047552049ab50dfcfceafdb2b82e6bc139d0
MatijaIlic97 <matijailic97@gmail.com>
2024-07-07 00:49:08 +0200

    login

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import judo from "/src/assets/img/judo.jpg"
-import {Card, CardBody, CardImg, CardImgOverlay, CardText, CardTitle} from "reactstrap";
 
 export default function Home() {
     return (
