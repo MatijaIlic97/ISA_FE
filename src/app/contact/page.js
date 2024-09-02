48582462813b7636876f6982694fcb1910359daa
MatijaIlic97 <matijailic97@gmail.com>
2024-09-03 00:36:04 +0200

    changed contact

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,27 @@
+import { FaPhone } from "react-icons/fa";
+
+
 export default function Contact() {
     return (
         <>
+            <h1>Lokacije</h1>
+            <h2>Banjica</h2>
+            <h3>Crnotravska 4</h3>
+            <h2>Zemun</h2>
+            <h3>Ševina 25a</h3>
+            <h2>Braće Jerković</h2>
+            <h3>Braće Jerković 123B</h3>
+            <br/>
+            <h1>Treneri</h1>
+            <h2>Banjica </h2>
+            <h3>Tasa</h3>
+            <p><FaPhone/>+381 60 1234532</p>
+            <h3>Mihajlo</h3>
+            <p><FaPhone/>+381 60 1234532</p>
+            <h3>Matija</h3>
+            <p><FaPhone/>+381 60 1234532</p>
+            <h3>Lazar</h3>
+            <p><FaPhone/>+381 60 1234532</p>
         </>
     )
 }
